25f221058568d4a23d01896f48a6ee37c9497a8b
Paul Röper <roperp8@gmail.com>
2019-10-21 11:41:45 +0200

    Inital

---
+++ New Unity Project/Assets/Scripts/ClickManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClickManager : MonoBehaviour
+{
+    public static int pets;
+    public TextMeshProUGUI display;
+
+    public void GetClicks()
+    {
+        pets += 1;
+        
+    }
+
+    private void Update()
+    {
+        display.text = ("You have " + pets + " Petpoints");
+    }
+}
---
+++ New Unity Project/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName;
+
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }  
+}
